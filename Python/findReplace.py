a866888f6d50bcf4cb23cfbc78d336a9d6ac9a7f
gaudard <gaudard@gmail.com>
2014-12-13 15:53:45 -0600

    minor updates

--- Python/findReplace.py
+++ Python/findReplace.py
@@ -32,6 +32,17 @@ with open('updatedPermissions','w+') as fout:
 
 
 
+teststring = "D:AI(A;CIID;0x1200a9;;;S-1-5-21-1573582526-416615227-727307700-1009)(A;OICIIOID;FR;;;S-1-5-21-1573582526-416615227-727307700-1009)(A;OICIID;0x1301bf;;;S-1-5-21-1573582526-416615227-727307700-1010)(A;OICIID;FA;;;S-1-5-21-1573582526-416615227-727307700-1011)(A;OICIID;FR;;;S-1-5-21-1573582526-416615227-727307700-1012)(A;OICIID;FA;;;SY)(A;OICIID;FA;;;BA)(A;OICIID;0x1200a9;;;BU)(A;CIID;LC;;;BU)(A;CIID;DC;;;BU)(A;OICIIOID;GA;;;CO)"
+
+r = re.findall('S-\d-\d-\d+-\d+-\d+-\d+-\w+', teststring)
+
+print "[+] Found SID!! ", r
+
+
+
+
+
+
 #### Closes files ####
 
 sids.close()
--- Python/regex.py
+++ Python/regex.py
@@ -7,21 +7,15 @@ import re
 print "[+] Starting"
 
 
-teststring = "D:AI(A;CIID;0x1200a9;;;S-1-5-21-1573582526-416615227-727307700-1009)(A;OICIIOID;FR;;;S-1-5-21-1573582526-416615227-727307700-1009)(A;OICIID;0x1301bf;;;S-1-5-21-1573582526-416615227-727307700-1010)(A;OICIID;FA;;;S-1-5-21-1573582526-416615227-727307700-1011)(A;OICIID;FR;;;S-1-5-21-1573582526-416615227-727307700-1012)(A;OICIID;FA;;;SY)(A;OICIID;FA;;;BA)(A;OICIID;0x1200a9;;;BU)(A;CIID;LC;;;BU)(A;CIID;DC;;;BU)(A;OICIIOID;GA;;;CO)"
+f = open('permissions', 'r')
+r = re.compile('S-\d-\d-\d+-\d+-\d+-\d+-\w+')
 
+for line in f:
+	if re.match('S-\d-\d-\d+-\d+-\d+-\d+-\w+', line):
+		print line
 
-'''
-with open(f, 'r') as f:
-	lines = f.readlines()
-'''
+f.close()
 
-
-r = re.findall('S-\d-\d-\d+-\d+-\d+-\d+-\w+', teststring)
-
-
-
-print "[+] Found SID!! ", r
-	
 
 	
 
