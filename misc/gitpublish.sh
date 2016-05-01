8a98d2298f62b3d2ee6d0ec5539004e690929bc9
gaudard <gaudard@gmail.com>
2016-05-01 05:06:40 -0400

    final version.. hopefully

--- misc/gitpublish.sh
+++ misc/gitpublish.sh
@@ -1,3 +1,10 @@
+#################################################
+# Created by @jgaudard  :: I don't twitter much 
+# bash script to publish to github, for the lazy...
+# Created: 30 April 2016    Edited: 30 April 2016
+# Version 1.0
+#################################################
+
 git add .
 read -p "What do you want to say?`echo $'\n> '`" comment
 git commit -m "$comment"
