92c39c0d85dd59e47b3bd0f3c938eab894d5bce8
gaudard <gaudard@gmail.com>
2017-11-22 06:54:51 -0600

    writes to file, track run time, randomizes

--- python/service_scoring.py
+++ python/service_scoring.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
 
-import subprocess, shlex, random
+import subprocess, shlex, random, re, time shutil
 
 targets = ["192.168.10.101","192.168.10.102","192.168.10.103"]
 ports = ["21","22","23","445"]
 scores = {"192.168.10.101":0,"192.168.10.102":0,"192.168.10.103":0}
+scoreFile = "/root/scores.txt"
 
 def scanner(ip, port)
-  cmd = "/usr/bin/nmap -n -sV -p" + port + " -Pn -oG - " + ip
+  cmd = "/usr/bin/nmap -n --max-retries 0 -T4 -sV -p" + port + " -Pn -oG - " + ip
   args = shlex.split(cmd)
   scan = subprocess.check_output(args)
   return scan.decode('utf-8')
   
 while True:
-  print(scores)
+  startTime = time.time()
+  random.shuffle(targets)
+  random.shuffle(ports)
+  #print(scores)
   random.shuffle(targets)
   for port in ports:
     for target in targets:
       if "open" in scanner(target, port):
-        scores[target] += 1
+        scores[target] += 2
+      else:
+        scores[target] -= 10
+  endTime = time.time()
+  with open(scoreFile, 'w') as outfile:
+    for target in scores:
+      line = target, scores[target]
+      outfile.write(re.sub("[()',]", '', str(line)))
+      outfile.write('\n')
+  shutil.copy(scoreFile, '/var/www/html/scores.txt')
+  print("Scores updated. Run time: ", endTime - startTime)
+  time.sleep(900)
